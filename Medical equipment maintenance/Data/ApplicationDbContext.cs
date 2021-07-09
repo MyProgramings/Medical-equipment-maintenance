b6558c1050f45cd3a8ac18ff2a5c44a9a2f394fa
Abdullah <Abdullah@DESKTOP-VMU2MBJ>
2021-07-09 04:27:54 -0700

    Add project files.

---
+++ Medical equipment maintenance/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medical_equipment_maintenance.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
